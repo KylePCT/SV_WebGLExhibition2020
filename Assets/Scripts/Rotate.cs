f59bbeccbfc83c8b3a8be3336d5ea9c9bab74988
KylePCT <ktugwell@live.co.uk>
2020-10-27 11:58:31 +0000

    Visuals

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -8,16 +8,23 @@ public class Rotate : MonoBehaviour
     public float bobSpeed = 5f; //Adjust this to change speed.
     public float bobHeight = 0.5f; //Adjust this to change how high it goes.
 
+    private Vector3 pos;
+
+    private void Start()
+    {
+        //Get the objects current position and put it in a variable so we can access it later with less code.
+        Vector3 pos = this.transform.position;
+    }
+
     void Update()
     {
         //Infinitely rotates on the y axis.
         transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
 
-            //Get the objects current position and put it in a variable so we can access it later with less code.
-            Vector3 pos = transform.position;
-            //Calculate what the new Y position will be.
-            float newY = Mathf.Sin(Time.time * bobSpeed);
-            //Set the object's Y to the new calculated Y.
-            transform.position = new Vector3(pos.x, newY, pos.z) * bobHeight;
+        //Calculate what the new Y position will be.
+        float newY = Mathf.Sin(Time.time * bobSpeed);
+        //Set the object's Y to the new calculated Y.
+        pos.y = newY;
+        this.transform.position = new Vector3(this.transform.position.x, (pos.y * bobHeight), this.transform.position.z);
     }
 }
